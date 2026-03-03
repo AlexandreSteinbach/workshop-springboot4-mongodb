0a47a4750cb7a94888942fd5457c8f440f14b587
AlexandreSteinbach <alexandresteinbach2@gmail.com>
2026-03-02 22:11:25 -0300

    DTO implementation

--- src/main/java/com/alexandresteinbach/workshopmongo/resources/UserResource.java
+++ src/main/java/com/alexandresteinbach/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.alexandresteinbach.workshopmongo.resources;
 
 import com.alexandresteinbach.workshopmongo.domain.User;
+import com.alexandresteinbach.workshopmongo.dto.UserDTO;
 import com.alexandresteinbach.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -18,8 +21,9 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list); //http ok: 200
+        List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO); //http ok: 200
     }
 }

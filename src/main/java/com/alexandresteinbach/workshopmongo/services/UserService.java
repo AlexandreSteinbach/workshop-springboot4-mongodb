987447a81bf477eb1767236869e7486d8b6ccfd9
AlexandreSteinbach <alexandresteinbach2@gmail.com>
2026-03-02 23:09:48 -0300

    findById

--- src/main/java/com/alexandresteinbach/workshopmongo/services/UserService.java
+++ src/main/java/com/alexandresteinbach/workshopmongo/services/UserService.java
@@ -2,6 +2,7 @@ package com.alexandresteinbach.workshopmongo.services;
 
 import com.alexandresteinbach.workshopmongo.domain.User;
 import com.alexandresteinbach.workshopmongo.repository.UserRepository;
+import com.alexandresteinbach.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +16,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        User user = repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found."));
+        return user;
     }
 }
